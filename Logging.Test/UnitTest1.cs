f0b904258813cf20d0fa65ea8880891f09591a9d
psmelser <paul.smelser@gmail.com>
2015-11-06 23:06:05 -0500

    needed a test for jenkins

---
+++ Logging.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Whatsnexx.Logging;
+
+namespace Logging.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Logger.RegisterDatabase("Data Source=(local);Integrated Security=SSPI;Initial Catalog=Logging;");
+            //Logger.ApplicationStartup();
+            //Logger.ApplicationTrace("Hello Baby");
+            //Logger.ExceptionHandled(new Exception(), "{json:fun}");
+        }
+    }
+}
